346b6125f0a1e153c6b43c35568c0400fedcfb93
彡 Dєƒƒiк 彡 <123771670+Nikita2275@users.noreply.github.com>
2026-04-09 15:48:44 +0300

    Add files via upload
    Неориентированный граф с N вершинами и M ребрами задан списком рёбер. Выведите его матрицу смежности.
    
    Входные данные
    В первой строке вводится 2 целых числа N и M  — число вершин и рёбер в графе (1≤N≤1000,0≤M≤n⋅(n−1)2)
    
    Выходные данные
    Выведите матрицу смежности графа: на пересечении i-й строки и j-го столбца должна стоять единица, если существует ребро из вершины с номером i в вершину с номером j, и ноль в противном случае.

---
+++ #1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "=== Graph Adjacency Matrix ===\n\n";
+
+    int n, m;
+    cout << "Enter number of vertices (n) and edges (m): ";
+    cin >> n >> m;
+
+    // Создаем матрицу смежности размером (n+1) x (n+1), заполненную нулями
+    vector<vector<int>> g(n + 1, vector<int>(n + 1, 0));
+
+    cout << "\nEnter " << m << " edges (u v):\n";
+    cout << "------------------------\n";
+
+    for (int i = 1; i <= m; i++) {
+        int u, v;
+        cout << "Edge " << i << ": ";
+        cin >> u >> v;
+
+        // Неориентированный граф - ставим 1 в обе стороны
+        g[u][v] = 1;
+        g[v][u] = 1;
+    }
+
+    cout << "\nAdjacency Matrix (" << n << "x" << n << "):\n";
+    cout << "------------------------\n";
+
+    // Выводим номера вершин (заголовки столбцов)
+    cout << "   ";
+    for (int j = 1; j <= n; j++) {
+        cout << " " << j;
+    }
+    cout << endl;
+
+    cout << "   ";
+    for (int j = 1; j <= n; j++) {
+        cout << "--";
+    }
+    cout << endl;
+
+    // Выводим матрицу
+    for (int i = 1; i <= n; i++) {
+        cout << i << " |";
+        for (int j = 1; j <= n; j++) {
+            cout << " " << g[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "------------------------\n";
+    cout << "\nProgram finished.\n";
+
+    return 0;
+}
