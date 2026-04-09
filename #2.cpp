3c80b8148d41965973ba44ad40f0fcac466a01f9
彡 Dєƒƒiк 彡 <123771670+Nikita2275@users.noreply.github.com>
2026-04-09 15:57:17 +0300

    Add files via upload
    Неориентированный граф с N вершинами задан матрицей смежности. Выведите его список смежности.
    
    Входные данные
    В первой строке вводится 1 натуральное число N — число вершин в графе (1≤N≤1000). В следующих N строках описан граф матрицей смежности: на пересечении i-й строки и j-го столбца будет стоять единица, если существует ребро из вершины с номером i в вершину с номером j, и ноль в противном случае.
    
    Выходные данные
    Выведите список смежности заданного графа: вывод должен состоять из N строк, в i-й строке должны быть перечислены номера вершин (в возрастающем порядке) с которыми смежна вершина номер i.

---
+++ #2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "=== Matrix to Adjacency List Converter ===\n\n";
+
+    int N;
+    cout << "Enter the number of vertices: ";
+    cin >> N;
+
+    vector<vector<int>> adj(N);
+
+    cout << "\nEnter the adjacency matrix (" << N << "x" << N << "):\n";
+    cout << "(enter 0 or 1 for each cell)\n";
+    cout << "------------------------\n";
+
+    for (int i = 0; i < N; ++i) {
+        cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < N; ++j) {
+            int x;
+            cin >> x;
+            if (x == 1) {
+                adj[i].push_back(j + 1);
+            }
+        }
+    }
+
+    cout << "\nAdjacency List (neighbors of each vertex):\n";
+    cout << "------------------------\n";
+
+    for (int i = 0; i < N; ++i) {
+        cout << "Vertex " << i + 1 << " -> ";
+        if (adj[i].empty()) {
+            cout << "(no neighbors)";
+        }
+        else {
+            for (int v : adj[i]) {
+                cout << v << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    cout << "------------------------\n";
+    cout << "\nProgram finished.\n";
+
+    return 0;
+}
